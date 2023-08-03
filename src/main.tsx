cc3c1356ab04cb9a9bfc96fd8d0013135b472ae9
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-08-03 16:18:04 -0300

    Atualização da correção do teste coverage

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
 
 ReactDOM
@@ -9,5 +10,6 @@ ReactDOM
   .render(
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+    ,
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,58 @@
+import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
 function Login() {
-  return <div>Login</div>;
+  const [passwordValidation, setPasswordValidation] = useState('');
+  const [emailValidation, setEmailValidation] = useState('');
+  // const navigation = useNavigate();
+
+  const validateSetPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setPasswordValidation(event.target.value);
+    // setEmailValidation(event.target.value);
+  };
+
+  const validateSetEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setEmailValidation(event.target.value);
+  };
+
+  const validateEmail = () => {
+    const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+    return regexEmail.test(emailValidation);
+  };
+  const disabledButton = () => {
+    return passwordValidation.length < 6 || validateEmail();
+  };
+
+  // const validateForms = () => {
+  //   return disabledButton() || validateEmail();
+  // };
+
+  return (
+    <form>
+      <label htmlFor="input-email">
+        <input
+          type="email"
+          name="email"
+          id="input-email"
+          data-testid="email-input"
+          onChange={ validateSetEmail }
+        />
+      </label>
+      <label htmlFor="input-password">
+        <input
+          type="password"
+          name=""
+          id="input-password"
+          data-testid="password-input"
+          minLength={ 6 }
+          onChange={ validateSetPassword }
+        />
+      </label>
+      <button type="submit" disabled={ disabledButton() }>Entrar</button>
+    </form>
+  );
 }
 
 export default Login;
