30ab5e964e0060f4f87b8a68434edc7ef6317f7c
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-08-03 23:37:49 -0300

    Requisito 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+import { useSelector } from 'react-redux';
+import { GlobalState } from '../types';
+
 function Header() {
+  const globalState = useSelector((state: GlobalState) => state);
   return (
     <div>
-      Header
+      <p data-testid="email-field">
+        Email:
+        {' '}
+        {globalState.user.email}
+      </p>
+      <p data-testid="total-field">
+        Despesas:
+        0
+      </p>
+      <p data-testid="header-currency-field">
+        BRL
+      </p>
     </div>
   );
 }
